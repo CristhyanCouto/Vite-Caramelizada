01f1cec3e89c51777599f67f2eece5da3334f574
Cristhyan Couto <cristhyancouto@gmail.com>
2024-10-12 00:09:08 -0300

    translations updates comboboxes

--- client/src/components/common/categoryComboBox.tsx
+++ client/src/components/common/categoryComboBox.tsx
@@ -100,7 +100,7 @@ function CategoryComboBoxMovies({
           aria-expanded={open}
           className="w-[200px] justify-between"
         >
-          {t("sortComboBox.selectGenre")}
+          {t("sortComboBox.genres")}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
--- client/src/components/common/sortComboBox.tsx
+++ client/src/components/common/sortComboBox.tsx
@@ -71,10 +71,7 @@ function SortComboBoxMovies({
           role="combobox"
           aria-expanded={open}
           className="w-[200px] justify-between"
-        >
-          {sortValue
-            ? sorts.find((sort) => sort.value === sortValue)?.label
-            : `${t("sortComboBox.selectSort")}`}
+        >{t("sortComboBox.filter")}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
