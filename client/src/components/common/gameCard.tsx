a2a3d803a67dcb69fcab2c6e9bd0d8a7998e4965
Cristhyan Couto <cristhyancouto@gmail.com>
2024-11-09 02:12:43 -0300

    update: client - game card layout fixed update

--- client/src/components/common/gameCard.tsx
+++ client/src/components/common/gameCard.tsx
@@ -22,7 +22,7 @@ export default function GameCard(props: GamesType) {
     <div>
       <div
         className="shadow p-2 m-5 h-[450px] w-72 rounded-sm
-        transition ease-in-out delay-150 bg-zinc-400
+        transition ease-in-out delay-150 bg-zinc-500
         duration-200
         group relative"
       >
@@ -37,11 +37,12 @@ export default function GameCard(props: GamesType) {
 
             {/*Showing my rating review in absolute position over the movie cover */}
             <div
-              className="absolute top-0 translate-y-[485%] translate-x-48 p-5 bg-red-500
-            rounded-full
-            block group-hover:hidden"
+              className="absolute top-0 translate-y-[430%] translate-x-[230%] p-5 bg-red-500
+              rounded-full
+              block group-hover:hidden
+              w-20 h-20"
             >
-              <p className="text-white text-2xl">
+              <p className="text-white text-3xl font-bold">
                 {" "}
                 {typeof props.my_rating === "number"
                   ? props.my_rating.toString().includes(".")
@@ -51,40 +52,42 @@ export default function GameCard(props: GamesType) {
               </p>
             </div>
 
+            {/* Showing First Plataform (The one I've played the game) */}
+            <div className="block group-hover:hidden">
+              <div className="absolute top-[76%] ml-5 left-0 z-20 transition-all duration-300">
+                <PlataformBackground plataform={props.fk_plataform01 ?? 0} />
+              </div>
+            </div>
+
+            {/* This div is responsible for showing the hover information and the dark background when hover */}
             <div
               className="absolute top-0 left-0 w-full h-full group-hover:bg-black bg-opacity-50
-            transition-all duration-300 group-hover:opacity
-            group-hover:bg-opacity-60"
+              transition-all duration-300 group-hover:opacity
+              group-hover:bg-opacity-70"
             >
-              {/* Showing First Plataform (The one I've played the game) */}
-              <div className="block group-hover:hidden">
-                <div className="absolute top-[76%] ml-5 left-0 z-20 transition-all duration-300">
-                  <PlataformBackground plataform={props.fk_plataform01 ?? 0} />
-                </div>
-              </div>
-
+              {/* This div is responsible for positioning the hover information*/}
               <div
                 className="hidden group-hover:block
-            absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
               >
-                {/*Showing Tittle based on language */}
+                {/*Showing Tittle based on languages en and pt */}
                 <div className="text-center font-bold text-white bg-red-500 rounded-sm py-3">
                   <h1 className="text-2xl">
                     {currentLanguage === "en" ? props.title_en : props.title_pt}
                   </h1>
                 </div>
 
-                {/*Showing Genre based on language */}
-                <div className="flex gap-4 my-4 text-black">
-                  <p className="grid bg-red-500 text-white rounded-sm w-24 shadow text-center align-center items-center p-2">
+                {/*Showing Genre based on language, only shows the first 2 genders */}
+                <div className="flex gap-4 my-4 font-bold text-center text-white items-center">
+                  <p className="bg-red-500 rounded-sm w-24 shadow p-2 h-full min-h-[6rem] items-center justify-center flex">
                     {t(`genre.${genre01}`)}
                   </p>
-                  <p className="grid bg-red-500 text-white rounded-sm w-24 shadow text-center align-center items-center p-2">
+                  <p className="bg-red-500 rounded-sm w-24 shadow p-2 h-full min-h-[6rem] items-center justify-center flex">
                     {t(`genre.${genre02}`)}
                   </p>
                 </div>
 
-                {/*Showing multiplayer */}
+                {/*Showing singleplayer, multiplayer and multiplayer-local with react-icons */}
                 <div className="flex justify-center gap-1 py-2 text-white bg-red-500 rounded-sm shadow mt-4">
                   {props?.singleplayer ? (
                     <div className="flex items-center">
@@ -109,10 +112,11 @@ export default function GameCard(props: GamesType) {
                   ) : null}
                 </div>
 
-                <div>
+                {/* Showing My Rating on hover */}
+                <div className="font-bold">
                   {props?.my_rating ? (
-                    <div className="flex justify-center gap-1 py-2 text-white bg-red-500 rounded-sm shadow mt-4 items-center">
-                      <p className="text-center text-3xl">
+                    <div className="grid grid-cols-3 justify-center gap-1 py-2 text-white bg-red-500 rounded-sm shadow mt-4 items-center">
+                      <p className="text-center text-3xl ml-7">
                         <FaStar />
                       </p>
                       <p className="text-center text-3xl">
@@ -120,7 +124,6 @@ export default function GameCard(props: GamesType) {
                           ? props.my_rating
                           : `${props.my_rating}.0`}
                       </p>
-                      <p className="text-center text-3xl">/10</p>
                     </div>
                   ) : null}
                 </div>
